580706871d3ce325b569fc1d7a80aae8aa050f52
Johan Östling <joos@wappo.se>
2025-08-17 13:53:32 +0200

    Update Secret model with ApiIgnore attribute, casts and fields

--- tests/Fakes/Models/Secret.php
+++ tests/Fakes/Models/Secret.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Wappo\LaravelSchemaApi\Tests\Fakes\Models;
 
-use Database\Factories\SecretFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Wappo\LaravelSchemaApi\Tests\Factories\SecretFactory;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,31 +13,38 @@ use Illuminate\Database\Eloquent\Model;
 use Wappo\LaravelSchemaApi\Attributes\ApiIgnore;
 use Wappo\LaravelSchemaApi\Concerns\HasDateFormat;
 
-#[ApiIgnore]
 /**
  *
  *
  * @property string $id
- * @property string $real_name
- * @property string $noc_list_name
- * @property bool $is_dead
+ * @property string $launch_code
+ * @property string $nuke_payload
+ * @property bool $is_armed
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property string|null $deleted_at
- * @method static \Database\Factories\SecretFactory factory($count = null, $state = [])
+ * @method static SecretFactory factory($count = null, $state = [])
  * @method static Builder<static>|Secret newModelQuery()
  * @method static Builder<static>|Secret newQuery()
  * @method static Builder<static>|Secret query()
  * @method static Builder<static>|Secret whereCreatedAt($value)
  * @method static Builder<static>|Secret whereDeletedAt($value)
  * @method static Builder<static>|Secret whereId($value)
- * @method static Builder<static>|Secret whereIsDead($value)
- * @method static Builder<static>|Secret whereNocListName($value)
- * @method static Builder<static>|Secret whereRealName($value)
+ * @method static Builder<static>|Secret whereIsArmed($value)
+ * @method static Builder<static>|Secret whereLaunchCode($value)
+ * @method static Builder<static>|Secret whereNukePayload($value)
  * @method static Builder<static>|Secret whereUpdatedAt($value)
  * @mixin \Eloquent
  */
+#[ApiIgnore]
 class Secret extends Model
 {
     use HasUuids, HasFactory, HasDateFormat;
+
+    protected function casts(): array
+    {
+        return [
+            'is_armed' => 'boolean',
+        ];
+    }
 }
